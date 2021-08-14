8593bf98926605b120be432d481864ad7dde3834
PaHer-Soft <patrickhernandez5384@gmail.com>
2021-08-14 11:26:28 -0600

    Agregando SideBar de videos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarPrincipalComponent } from './componentes/navbar-principal/navbar-principal.component';
 import { IonicModule } from '@ionic/angular';
+import { SidebarVideosComponent } from './componentes/sidebar-videos/sidebar-videos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarPrincipalComponent
+    NavbarPrincipalComponent,
+    SidebarVideosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/sidebar-videos/sidebar-videos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-videos',
+  templateUrl: './sidebar-videos.component.html',
+  styleUrls: ['./sidebar-videos.component.css']
+})
+export class SidebarVideosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
